20c3d7d98edb0aebf30a621038ccf76eb8bb7955
ASoldo <soldo.andrej@gmail.com>
2024-03-26 15:42:13 +0100

    Add Solana Wallet for Nuxt3

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,6 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt"],
+  vite: {
+    esbuild: {
+      target: "esnext",
+    },
+    build: {
+      target: "esnext",
+    },
+    optimizeDeps: {
+      include: ["@solana/web3.js", "buffer"],
+      esbuildOptions: {
+        target: "esnext",
+      },
+    },
+    define: {
+      "process.env.BROWSER": true,
+    },
+  },
   pinia: {
     storesDirs: ["./stores/**"],
   },
--- web/pages/index.vue
+++ web/pages/index.vue
@@ -2,11 +2,27 @@
   <div>
     <br />
     <NuxtLink id="about-link" to="/about">About</NuxtLink>
+    <ClientOnly>
+      <WalletMultiButton dark />
+    </ClientOnly>
     <h1 class="text-3xl font-bold underline bg-red-500">Hello world!</h1>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref, watch } from "vue";
 import { useCounterStore } from "@/stores/counter";
 const store = useCounterStore();
+import { WalletMultiButton, useWallet } from "solana-wallets-vue";
+const { wallet, connected } = useWallet();
+
+watch(connected, (newConnectedStatus, prevConnectedStatus) => {
+  if (newConnectedStatus) {
+    console.log('Wallet connected');
+  } else {
+    console.log('Wallet disconnected');
+  }
+});
+
+const soldo = ref(0);
 </script>
